11c0a72ba33c9ffe40f78eb75357065a0ad7fd9e
Festus <festusowumi@gmail.com>
2020-01-31 21:35:00 +0100

    initial commit

--- admin_side_flutter_ecommerce_app-master/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:shop_app_admin/screens/admin.dart';
-
-void main(){
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Admin(),
-  ));
-}
